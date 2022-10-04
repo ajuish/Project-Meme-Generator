113584e8225978726dd9659da8f9d4ee2d577981
Alex Ju <aju217@gmail.com>
2022-10-04 14:37:08 -0400

    add memes to list

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 document.addEventListener("DOMContentLoaded", fetchMemes)
 
+//create reference constants
+const memeList = document.getElementById("meme-list");
+const memeImg = document.getElementById("meme-image");
+const memeDesc = document.getElementById("meme-desc");
+const memeForm = document.getElementById("meme-form");
+
+//create empty meme data array
 let rawMemeData = []
 
 function addImageName() {
@@ -12,11 +19,18 @@ function addImageName() {
 function fetchMemes(){
     fetch("https://api.imgflip.com/get_memes")
     .then(resp => resp.json())
-    .then(data => {
-        rawMemeData = data;
-        memeData = rawMemeData.data.memes
-        console.log(memeData)
+    .then(apidata => {
+        memeData = apidata.data.memes.slice(1,11);
+        addMemes(memeData)
     })
     .catch((error)=> console.log(error.message))
 }
 
+function addMemes(memeData){
+    const memeList = document.getElementById("meme-list");
+    memeData.forEach((meme) => {
+        const memeName = document.createElement("h5");
+        memeName.textContent = meme.name;
+        memeList.append(memeName)
+    })
+}
