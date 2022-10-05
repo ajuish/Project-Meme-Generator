c3103af8ab0fa4bd5caa71dd76edd71304cdd209
Alex Ju <aju217@gmail.com>
2022-10-05 15:30:20 -0400

    add persistence to desc and likes

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", fetchMemes)
 
 //create empty meme data array
 let memeData = []
+let currentMeme
 
 //fetch meme data from public API
 function fetchMemes(){
@@ -26,6 +27,8 @@ function addMemes(memeData){
     const memeList = document.getElementById("meme-list");
     memeData.forEach((meme) => {
         const memeName = document.createElement("h5");
+        meme.desc = "";
+        meme.likes = 0;
         memeName.textContent = meme.name;
         memeName.addEventListener("click", ()=>showMeme(meme))
         memeList.append(memeName)
@@ -33,20 +36,25 @@ function addMemes(memeData){
 }
 
 function showMeme(meme){
+    currentMeme = meme
     const memeImg = document.getElementById("meme-image");
     const memeTitle = document.getElementById('meme-name');
     const memeDesc = document.getElementById("meme-desc");
+    const likeCount = document.getElementById("like-count")
+    likeCount.textContent = meme.likes;
     memeImg.src = meme.url;
     memeTitle.textContent = meme.name;
-    memeDesc.textContent = "";
-}
+    memeDesc.textContent = meme.desc;
+} 
 
 function addMemeText(){
     const memeForm = document.getElementById("meme-form");
+    //add event  listener to meme form
     memeForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const memeDesc = document.getElementById('meme-desc');
-    memeDesc.textContent = document.getElementById('meme-text').value;
+    currentMeme.desc = document.getElementById('meme-text').value;
+    memeDesc.textContent = currentMeme.desc;
     memeForm.reset();
 })}
 
@@ -54,6 +62,7 @@ function addLikes(){
   let likeCount = document.getElementById('like-count')
   document.addEventListener('keydown', (event) => {
     if(event.key === "ArrowRight"){
-    likeCount.textContent++
+    currentMeme.likes = Number(currentMeme.likes) + 1;
+    likeCount.textContent = currentMeme.likes;
 }
   })}
